538a10ed3aeab53d1d950bb683a243cfb2cf7b5d
534-PC\534 <nakatabjy@gmail.com>
2018-01-23 13:38:42 +0900

    ------ 입력과출력

--- src/GitProjPractice.java
+++ src/GitProjPractice.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 
 public class GitProjPractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 			System.out.println("안녕?");
+			System.out.println("응 안녕");
+			System.out.println("숫자입력");
+			int a = sc.nextInt();
+			System.out.println("숫자또입력");
+			int b = sc.nextInt();
+			
+			
+			//출력
+			System.out.println("입력한숫자"+a+"그리고"+b);
+			
+			
 	}
 
 }
